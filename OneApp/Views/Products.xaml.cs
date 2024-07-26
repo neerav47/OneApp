1cdb034205b21ebfdbd364903a61ff1139ee5bff
neerav <neeravsankla@outlook.com>
2024-07-26 10:01:11 -0400

    Added tabbar, updated splash screen and MainPage UI

---
+++ OneApp/Views/Products.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OneApp.Views;
+
+public partial class Products : ContentPage
+{
+	public Products()
+	{
+		InitializeComponent();
+	}
+}
---
+++ OneApp/Views/Settings.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OneApp.Views;
+
+public partial class Settings : ContentPage
+{
+	public Settings()
+	{
+		InitializeComponent();
+	}
+}
