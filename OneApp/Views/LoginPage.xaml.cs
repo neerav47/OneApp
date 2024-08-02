99473db5498533b7bc019d2f5d7b2c7fd74d41a6
Neerav Patel Sankla <136021123+nsankla@users.noreply.github.com>
2024-08-02 01:25:37 -0400

    Add login page (#6)

---
+++ OneApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace OneApp.Views;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+
+	void OnForgotPasswordTap(object sender, EventArgs eventArgs)
+	{
+		DisplayAlert("Forgot password", "Are you sure to change password", "Ok", "Cancel");
+	}
+}
