0a89b2d4299bf373601965c3e45f508ccb29427c
Neerav <neeravsankla@outlook.com>
2024-09-29 00:55:54 -0400

    Add "TReceipt and TSaleItem"

---
+++ OneApp.Data/Models/TSaleItem.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace OneApp.Data.Models;
+
+[Index(nameof(ReceiptId), Name = "IX_ReceiptId")]
+public sealed class TSaleItem
+{
+    [Key]
+    [Required]
+    public Guid Id { get; set; }
+
+    [Required]
+    public Guid ReceiptId { get; set; }
+
+    [Required]
+    public Guid TenantId { get; set; }
+
+    [Required]
+    public Guid ProductId { get; set; }
+
+    [Required]
+    public decimal Quantity { get; set; }
+
+    [Required]
+    public int UnitPrice { get; set; }
+
+    [Required]
+    public bool IsDeleted { get; set; }
+
+    [Required]
+    public DateTime CreatedDate { get; set; }
+
+    [Required]
+    public Guid CreatedBy { get; set; }
+
+    [Required]
+    public DateTime LastUpdatedDate { get; set; }
+
+    [Required]
+    public Guid LastUpdatedBy { get; set; }
+}
